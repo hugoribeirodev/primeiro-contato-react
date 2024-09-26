13c0280274633a2d5ef132c9a02322456c090bcc
Hugo-Ribeiro1 <hugorsousa44@gmail.com>
2024-09-26 13:06:53 -0300

    primeiro-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Style from "./Header.module.css";
+
+function Header(){
+    return(
+
+       <>
+            <div className={Style.cabecalho}>
+                <h1>Header da página</h1>
+            </div>
+       
+       </> 
+
+    )
+}
+export default Header
---
+++ src/components/meio.jsx
@@ -0,0 +1,13 @@
+import Style from "meio.module.css";
+
+function meio (){
+    return(
+        <>
+        <div className={Style.Metade}>
+            <h1>Meio</h1>
+        </div>
+        </>
+    )
+}
+
+export default Meio
